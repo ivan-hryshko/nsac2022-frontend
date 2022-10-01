1e3eae5016f61921754ed01a3adeac5a403405e7
ivan.hryshko <ihryshko@datrics.ai>
2022-10-01 22:35:06 +0300

    added space image

---
+++ src/components/space-image/space-image.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="space-image"
+  >
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SpaceImage',
+  props: {
+    position: {
+      type: String,
+      default: 'left',
+    },
+  },
+  setup(props) {
+    const styleOptions = computed(() => {
+      return {
+        'background-position': props.position,
+      }
+    })
+
+    return {
+      styleOptions,
+    }
+  },
+}
+</script>
+
+<style>
+@import './space-image';
+</style>
