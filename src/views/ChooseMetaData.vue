9125ad2a84669f171214d17aa5bf77df89a194f4
ivan.hryshko <ihryshko@datrics.ai>
2022-10-02 11:25:55 +0300

    changed carousel slide count

--- src/views/ChooseMetaData.vue
+++ src/views/ChooseMetaData.vue
@@ -17,48 +17,55 @@
         </div>
       </div>
     </div>
-    <carousel-3d :height="730" :width="520">
-      <slide :index="0" color="red">
-        <div class="image-poster">
-          <div class="image-poster__image-wrapped">
-            <img
-              class="image-poster__image"
-              src="@/assets/ImageSet/JNCE_2022229_44C00074_V01-mapprojected.png"
-            >
+    <div class="carousel-wrapped">
+      <carousel-3d
+        :height="730"
+        :width="520"
+        :space="400"
+        :display="3"
+      >
+        <slide :index="0" color="red">
+          <div class="image-poster">
+            <div class="image-poster__image-wrapped">
+              <img
+                class="image-poster__image"
+                src="@/assets/ImageSet/JNCE_2022229_44C00074_V01-mapprojected.png"
+              >
+            </div>
           </div>
-        </div>
-      </slide>
-      <slide :index="1">
-        <div class="image-poster">
-          <div class="image-poster__image-wrapped">
-            <img
-              class="image-poster__image"
-              src="@/assets/ImageSet/JNCE_2022229_44C00074_V01-blue.png"
-            >
+        </slide>
+        <slide :index="1">
+          <div class="image-poster">
+            <div class="image-poster__image-wrapped">
+              <img
+                class="image-poster__image"
+                src="@/assets/ImageSet/JNCE_2022229_44C00074_V01-blue.png"
+              >
+            </div>
           </div>
-        </div>
-      </slide>
-      <slide :index="2">
-        <div class="image-poster">
-          <div class="image-poster__image-wrapped">
-            <img
-              class="image-poster__image"
-              src="@/assets/ImageSet/JNCE_2022229_44C00074_V01-blue.png"
-            >
+        </slide>
+        <slide :index="2">
+          <div class="image-poster">
+            <div class="image-poster__image-wrapped">
+              <img
+                class="image-poster__image"
+                src="@/assets/ImageSet/JNCE_2022229_44C00074_V01-blue.png"
+              >
+            </div>
           </div>
-        </div>
-      </slide>
-      <slide :index="3">
-        <div class="image-poster">
-          <div class="image-poster__image-wrapped">
-            <img
-              class="image-poster__image"
-              src="@/assets/ImageSet/JNCE_2022229_44C00074_V01-red.png"
-            >
+        </slide>
+        <slide :index="3">
+          <div class="image-poster">
+            <div class="image-poster__image-wrapped">
+              <img
+                class="image-poster__image"
+                src="@/assets/ImageSet/JNCE_2022229_44C00074_V01-red.png"
+              >
+            </div>
           </div>
-        </div>
-      </slide>
-    </carousel-3d>
+        </slide>
+      </carousel-3d>
+    </div>
   </div>
 </template>
 
@@ -84,6 +91,11 @@
 <style>
   .slide {
     /* background-color: red; */
+  }
+
+  .carousel-wrapped {
+    width: 100%;
+
   }
 
   .carousel-3d-slide {
